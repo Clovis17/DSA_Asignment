9d0d539e8cd1a8ce45d8eb61ef9b62800f1fd2f1
Clovis17 <atikiclovis0@gmail.com>
2025-03-18 15:08:05 +0300

    Add files via upload

---
+++ Traverse_array.java
@@ -0,0 +1,9 @@
+public class Traverse_array {
+    public static void main (String [] args){
+        String [] name = {"John","Mark","Ivan","Jona","Smith","Grace","Angella","Christine"};
+
+        for(int i=0; i<name.length; i++){
+            System.out.println(name[i]);
+        }
+    }
+}
